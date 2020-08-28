1a3bc3b404485d666c80621dc0b5f5277421a674
godcong <jumbycc@163.com>
2020-08-28 16:54:12 +0800

    add linkrest

--- model/model.go
+++ model/model.go
@@ -7,7 +7,7 @@ import (
 )
 
 type BaseModel struct {
-	ID        string     `xorm:"uuid pk comment(默认主键)"`
+	ID        string     `xorm:"id uuid pk comment(默认主键)"`
 	CreatedAt time.Time  `xorm:"created comment(创建时间) 'created_at'"`
 	UpdatedAt time.Time  `xorm:"updated comment(更新时间) 'updated_at'" `
 	DeletedAt *time.Time `xorm:"deleted comment(删除时间) 'deleted_at'"`
