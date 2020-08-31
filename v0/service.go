93f8fc27259f8e7e2b233f26dcee6d0e2ce80413
godcong <jumbycc@163.com>
2020-08-31 18:10:58 +0800

    add cache

--- v0/service.go
+++ v0/service.go
@@ -30,13 +30,47 @@ func Register(db *gorm.DB, group *gin.RouterGroup, cache *persistence.InMemorySt
 	_v0.db = db
 	_v0.cache = cache
 	_v0.total(group)
+	_v0.query(group)
 }
 
 func (s service) total(group *gin.RouterGroup) {
 	group.GET("/show", cache.CachePage(s.cache, time.Minute, func(ctx *gin.Context) {
 		page := model.Page(ctx.Request, new([]model.Media))
 
-		find, err := page.Find(s.db.Table("media"))
+		find, err := page.Find(s.db.Model(model.Media{}))
+		if err != nil {
+			log.Errorw("find data error", "error", err)
+			FailedJSON(ctx, "data not found")
+			return
+		}
+
+		ctx.JSON(http.StatusOK, find)
+	}))
+}
+
+func (s service) query(group *gin.RouterGroup) {
+	group.GET("/query", cache.CachePage(s.cache, time.Minute, func(ctx *gin.Context) {
+		page := model.Page(ctx.Request, new([]model.Media))
+		m := s.db.Model(model.Media{})
+		if ctx.Query("video_no") != "" {
+			infos := s.db.Model(model.Info{}).Where("video_no=?", ctx.Query("video_no")).Select("id")
+			m = m.Where("media.info_id in (?)", infos)
+		}
+
+		if ctx.Query("intro") != "" {
+			infos := s.db.Model(model.Info{}).Where("intro=?", ctx.Query("intro")).Select("id")
+			m = m.Where("media.info_id in (?)", infos)
+		}
+
+		if ctx.Query("hash") != "" {
+			files := s.db.Model(model.File{}).Where("thumb_hash=?", ctx.Query("hash")).
+				Or("poster_hash=?", ctx.Query("hash")).
+				Or("source_hash=?", ctx.Query("hash")).
+				Or("m3u8_hash=?", ctx.Query("hash")).Select("id")
+			m = m.Where("media.file_id in (?)", files)
+		}
+
+		find, err := page.Find(m)
 		if err != nil {
 			log.Errorw("find data error", "error", err)
 			FailedJSON(ctx, "data not found")
