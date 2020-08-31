7ca0a3a7055df582334a2f749fe11600ef597afb
godcong <jumbycc@163.com>
2020-08-31 17:01:30 +0800

    add cache

--- model/file.go
+++ model/file.go
@@ -10,8 +10,8 @@ type File struct {
 	PosterHash string `xorm:"poster_hash" json:"poster_hash"`
 	SourcePath string `xorm:"source_path" json:"source_path"` //原片地址
 	SourceHash string `xorm:"source_hash" json:"source_hash"`
-	M3U8       string `gorm:"m3u8" json:"m3u8"`           //M3U8名
-	M3U8Path   string `xorm:"m3u8_path" json:"m3u8_path"` //切片地址
+	M3U8Index  string `gorm:"m3u8_index" json:"m3u8_index"` //M3U8名
+	M3U8Path   string `xorm:"m3u8_path" json:"m3u8_path"`   //切片地址
 	M3U8Hash   string `xorm:"m3u8_hash" json:"m3u8_hash"`
 }
 
