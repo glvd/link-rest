21b455818ee4fb0f46422d8fb725254a4b2d820b
godcong <jumbycc@163.com>
2020-08-31 14:21:24 +0800

    add linkrest

---
+++ model/file.go
@@ -0,0 +1,13 @@
+package model
+
+type File struct {
+	BaseModel `xorm:"extends" json:"-"`
+	Thumb     Hash `xorm:"thumb_hash" json:"thumb_hash"`   //缩略图
+	Poster    Hash `xorm:"poster_hash" json:"poster_hash"` //海报地址
+	Source    Hash `xorm:"source_hash" json:"source_hash"` //原片地址
+	M3U8      Hash `xorm:"m3u8_hash" json:"m3u8_hash"`     //切片地址
+}
+
+func init() {
+	RegisterTable(&File{})
+}
--- model/info.go
+++ model/info.go
@@ -1,13 +1,35 @@
 package model
 
 type Info struct {
-	BaseModel `xorm:"extends" json:"-"`
-	Thumb     Hash `xorm:"thumb_hash" json:"thumb_hash"`   //缩略图
-	Poster    Hash `xorm:"poster_hash" json:"poster_hash"` //海报地址
-	Source    Hash `xorm:"source_hash" json:"source_hash"` //原片地址
-	M3U8      Hash `xorm:"m3u8_hash" json:"m3u8_hash"`     //切片地址
-}
-
-func init() {
-	RegisterTable(&Info{})
+	BaseModel    `xorm:"extends" json:"-"`
+	VideoNo      string      `xorm:"video_no" json:"video_no"`           //编号
+	Intro        string      `xorm:"varchar(2048)" json:"intro"`         //简介
+	Alias        StringArray `xorm:"json" json:"alias"`                  //别名，片名
+	ThumbHash    string      `xorm:"thumb_hash" json:"thumb_hash"`       //缩略图
+	PosterHash   string      `xorm:"poster_hash" json:"poster_hash"`     //海报地址
+	SourceHash   string      `xorm:"source_hash" json:"source_hash"`     //原片地址
+	M3U8Hash     string      `xorm:"m3u8_hash" json:"m3u8_hash"`         //切片地址
+	Key          string      `xorm:"key"  json:"key"`                    //秘钥
+	M3U8         string      `xorm:"m3u8" json:"m3u8"`                   //M3U8名
+	Role         StringArray `xorm:"json" json:"role"`                   //主演
+	Director     string      `xorm:"director" json:"director"`           //导演
+	Systematics  string      `xorm:"systematics" json:"systematics"`     //分级
+	Season       string      `xorm:"season" json:"season"`               //季
+	TotalEpisode string      `xorm:"total_episode" json:"total_episode"` //总集数
+	Episode      string      `xorm:"episode" json:"episode"`             //集数
+	Producer     string      `xorm:"producer" json:"producer"`           //生产商
+	Publisher    string      `xorm:"publisher" json:"publisher"`         //发行商
+	Type         string      `xorm:"type" json:"type"`                   //类型：film，FanDrama
+	Format       string      `xorm:"format" json:"format"`               //输出格式：3D，2D,VR(VR格式：Half-SBS：左右半宽,Half-OU：上下半高,SBS：左右全宽)
+	Language     string      `xorm:"language" json:"language"`           //语言
+	Caption      string      `xorm:"caption" json:"caption"`             //字幕
+	Group        string      `xorm:"group" json:"group"`                 //分组
+	Index        string      `xorm:"index" json:"index"`                 //索引
+	ReleaseDate  string      `xorm:"release_date" json:"release_date"`   //发行日期
+	Sharpness    string      `xorm:"sharpness" json:"sharpness"`         //清晰度
+	Series       string      `xorm:"series" json:"series"`               //系列
+	Tags         StringArray `xorm:"json tags" json:"tags"`              //标签
+	Length       string      `xorm:"length" json:"length"`               //时长
+	Sample       StringArray `xorm:"json sample" json:"sample"`          //样板图
+	Uncensored   bool        `xorm:"uncensored" json:"uncensored"`       //有码,无码
 }
