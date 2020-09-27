1f7e50a0c2a7c304939e0c14964df5270c169b25
godcong <jumbycc@163.com>
2020-09-27 17:59:18 +0800

    update

---
+++ config/config.go
@@ -0,0 +1,21 @@
+package config
+
+type Config struct {
+	Port int //handle port
+}
+
+func defaultConfig() *Config {
+	return &Config{
+		Port: 18080,
+	}
+}
+
+type Options func(config *Config)
+
+func ParseConfig(opts ...Options) *Config {
+	cfg := defaultConfig()
+	for _, opt := range opts {
+		opt(cfg)
+	}
+	return cfg
+}
