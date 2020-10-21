f3f13a6959e7d5d95f19a28a34e75b421d314ce8
godcong <jumbycc@163.com>
2020-10-21 18:21:31 +0800

    update

--- db/database.go
+++ db/database.go
@@ -21,6 +21,30 @@ type SQLConnect struct {
 	Param    string
 	Location string
 }
+type mysqlInfo struct {
+	SQLType  string
+	Username string
+	Password string
+	Addr     string
+	Port     string
+	Schema   string
+	Param    string
+	Location string
+}
+
+func (m mysqlInfo) Type() string {
+	return "mysql"
+}
+
+func (m mysqlInfo) String() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s%sloc=%s&charset=utf8&parseTime=true",
+		m.Username, m.Password, m.Addr, m.Port, m.Schema, m.Param, url.QueryEscape(m.Location),
+	)
+}
+
+func (m mysqlInfo) ConnectParams() (string, string) {
+	return m.Type(), m.String()
+}
 
 type Connectable interface {
 	Type() string
@@ -28,11 +52,25 @@ type Connectable interface {
 	ConnectParams() (string, string)
 }
 
-func ParseFromMap(m extmap.Map) *SQLConnect {
-	c := defaultSQLConnect()
-	if m == nil {
-		return c
+func ParseFromMap(m extmap.Map) Connectable {
+	switch m.GetStringD("SQLType", "mysql") {
+	default:
+		return parseMysql(m)
 	}
+}
+
+func parseMysql(m extmap.Map) *mysqlInfo {
+	c := &mysqlInfo{
+		SQLType:  "mysql",
+		Username: "root",
+		Password: "111111",
+		Addr:     "127.0.0.1",
+		Port:     "3306",
+		Schema:   "linker",
+		Param:    "?",
+		Location: "Asia/Shanghai",
+	}
+
 	c.SQLType = m.GetStringD("SQLType", c.SQLType)
 	c.Username = m.GetStringD("Username", c.Username)
 	c.Password = m.GetStringD("Password", c.Password)
@@ -44,40 +82,12 @@ func ParseFromMap(m extmap.Map) *SQLConnect {
 	return c
 }
 
-func defaultSQLConnect() *SQLConnect {
-	return &SQLConnect{
-		SQLType:  "mysql",
-		Username: "root",
-		Password: "111111",
-		Addr:     "127.0.0.1",
-		Port:     "3306",
-		Schema:   "linker",
-		Param:    "?",
-		Location: "Asia/Shanghai",
-	}
-}
-
 func New(c Connectable) (*gorm.DB, error) {
-
 	db, err := gorm.Open(mysql.Open(c.String()), nil)
 	if err != nil {
 		return nil, fmt.Errorf("connect db error:%w", err)
 	}
 	return db, nil
-}
-
-func (c SQLConnect) Type() string {
-	return c.SQLType
-}
-
-func (c SQLConnect) String() string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s%sloc=%s&charset=utf8&parseTime=true",
-		c.Username, c.Password, c.Addr, c.Port, c.Schema, c.Param, url.QueryEscape(c.Location),
-	)
-}
-
-func (c SQLConnect) ConnectParams() (string, string) {
-	return c.Type(), c.String()
 }
 
 func OpenSqlite() {
